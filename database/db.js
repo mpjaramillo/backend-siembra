7b5753dcbea08d51959886b388131b8ad70b8ba2
Jean Paul Mosquera <jeanpaulmosquera1996@hotmail.com>
2020-02-07 11:23:27 -0500

    updating db details

--- database/db.js
+++ database/db.js
@@ -1,4 +1,4 @@
 module.exports = {
     //db: 'mongodb+srv://admin:root@cluster0-c0uxz.mongodb.net/test?retryWrites=true&w=majority'
-    db: 'mongodb://localhost:27017/reactdb'
+    db: 'mongodb+srv://admin:root@cluster0-c0uxz.mongodb.net/test?retryWrites=true&w=majority'
 };
