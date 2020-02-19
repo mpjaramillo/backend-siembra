ad5987874b445b03e2ade56007f82f17dce23691
mpjaramillo <mapujaramillo8@gmail.com>
2020-02-18 23:00:57 -0500

    prueba

--- database/conectionDB.js
+++ database/conectionDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb://admin:root@cluster0-shard-00-00-8zgqm.mongodb.net:27017,cluster0-shard-00-01-8zgqm.mongodb.net:27017,cluster0-shard-00-02-8zgqm.mongodb.net:27017/siembra?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
-
+//const URI = "mongodb://admin:root@cluster0-5h1ng.mongodb.net:27017,cluster0-5h1ng.mongodb.net:27017,cluster0-5h1ng.mongodb.net:27017/registro?ssl=true&replicaSet=cluster0-5h1ng&authSource=admin";
+const URI = "mongodb://admin:root@cluster0-shard-00-00-5h1ng.mongodb.net:27017,cluster0-shard-00-01-5h1ng.mongodb.net:27017,cluster0-shard-00-02-5h1ng.mongodb.net:27017/registro?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 const connectDB = async() => {
     await mongoose.connect(URI, {
         useUnifiedTopology: true,
